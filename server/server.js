a08e53081362dc5253d2a98a7c674e8960bab095
Simeon Lee <simeonhlee@gmail.com>
2016-08-30 10:18:40 -0700

    Update server to look at process.env for config variables

--- server/server.js
+++ server/server.js
@@ -59,8 +59,8 @@ db.once('open', function() {
 
 
 passport.use(new FacebookStrategy({
-    clientID: config.FACEBOOK_APP_ID,
-    clientSecret: config.FACEBOOK_APP_SECRET,
+    clientID: process.env.FACEBOOK_APP_ID || config.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET || config.FACEBOOK_APP_SECRET,
     // callbackURL: "http://www.example.com/auth/facebook/callback"
     callbackURL: "localhost:8080/auth/facebook/callback"
   },
