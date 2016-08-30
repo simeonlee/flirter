362c9c259e896261310afafc77bb2884a106f941
Simeon Lee <simeonhlee@gmail.com>
2016-08-30 11:08:39 -0700

    Implement ask for greater user permissions

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ db.once('open', function() {
     this.findOne({_id: profile.id}, function(err, result) {
       if (!result) {
         console.log(profile);
-        userObj.name = profile.displayName;
+        userObj.name = profile.displayName.split(' ')[0];
         userObj.save(cb);
       } else {
         cb(err, result);
@@ -64,7 +64,9 @@ db.once('open', function() {
   var app = express();
   app.use(express.static(__dirname + '/../client'));
   app.use(bodyParser.json());
+  app.use(express.session({ secret: 'secret' }));
   app.use(passport.initialize());
+  app.use(passport.session());
 
   var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
@@ -75,7 +77,9 @@ db.once('open', function() {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
       // callbackURL: "http://www.example.com/auth/facebook/callback"
-      callbackURL: "https://limitless-stream-28526.herokuapp.com/auth/facebook/callback"
+      callbackURL: "https://limitless-stream-28526.herokuapp.com/auth/facebook/callback",
+      // https://developers.facebook.com/docs/graph-api/reference/v2.5/user
+      profileFields: ['id', 'displayName', 'photos', 'email', 'bio', 'birthday', 'cover', 'education', 'first_name', 'last_name', 'gender', 'interested_in', 'is_verified', 'link', 'location', 'website', 'work']
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOrCreate(profile, function(err, user) {
@@ -143,7 +147,7 @@ db.once('open', function() {
   // Redirect the user to Facebook for authentication.  When complete,
   // Facebook will redirect the user back to the application at
   //     /auth/facebook/callback
-  app.get('/auth/facebook', passport.authenticate('facebook'));
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['user_friends', 'email', 'user_likes'] }));
 
   // Facebook will redirect the user to this URL after approval.  Finish the
   // authentication process by attempting to obtain an access token.  If
